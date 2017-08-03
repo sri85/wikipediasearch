5af0e7691221c043fa752dd48dbe8bfa3d0ad34a
sri85 <sripathi@roxtr.com>
2017-08-03 23:07:03 +0100

    First commit for wiki search

---
+++ scripts/wikipedia.js
@@ -0,0 +1,35 @@
+$('document').ready(function(){
+  $('#search').click(function(){
+    let searchTerm = ($("#searchterm").val());
+    if (searchTerm !== ""){
+      $("#searchresults").show();
+     getWikiArticles(searchTerm);
+    }
+
+  });
+
+  function getWikiArticles(searchTerm) {
+    let wikiApi = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch="+searchTerm+"&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=10"
+    $.ajax({url: wikiApi,
+    success: (articles) => {
+
+      let keys =(Object.keys(articles["query"]["pages"]));
+
+        for(let i =0; i<keys.length; i++){
+          let key = keys[i];
+
+            $("#searchresults").append('<a href=https://en.wikipedia.org/?curid='+[key]+' class="list-group-item" target="_blank">'
+              +'<h4 class="list-group-item-heading">'+articles["query"]["pages"][key]["title"]+'</h4>'+
+              '<p class="list-group-item-text">'+articles["query"]["pages"][key]["extract"]+'</p>'
+            +'</a>')
+
+        }
+      
+
+  },error: function(err){
+
+  },dataType: "jsonp"});
+}
+return false;
+
+});
